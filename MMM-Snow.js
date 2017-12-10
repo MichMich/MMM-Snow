ce4e30059023fceb67ba92c61b5bcccd5f31573a
Michael Teeuw <michael@xonaymedia.nl>
2017-12-10 10:44:29 +0100

    Fix flake sizing.

--- MMM-Snow.js
+++ MMM-Snow.js
@@ -22,13 +22,20 @@ Module.register("MMM-Snow",{
 		var flake, jiggle, size;
 
 		for(var i = 0; i < this.config.flakeCount; i++) {
-			flake = document.createElement("div")
-			flake.className = "snow-flake"
+
+			size = (Math.random() * 0.75) + 0.25;
+			flakeImage = document.createElement("div")
+			flakeImage.className = "flake" + (Math.round(Math.random() * 2) + 1);
+			flakeImage.style.transform = "scale(" + size +", "+size+")";
+			flakeImage.style.opacity = size;
+
+			flake = document.createElement("div");
+			flake.className = "snow-flake";
 
 			jiggle = document.createElement("div");
 			jiggle.style.animationDelay = (Math.random() * 4) + "s";
 			jiggle.style.animationDuration = ((Math.random() * 30) + 30) + "s";
-			jiggle.className = "flake" + (Math.round(Math.random() * 2) + 1);
+			jiggle.appendChild(flakeImage);
 
 			size = (Math.random() * 0.75) + 0.25;
 			jiggle.style.transform = "scale(" + size +", "+size+")";
